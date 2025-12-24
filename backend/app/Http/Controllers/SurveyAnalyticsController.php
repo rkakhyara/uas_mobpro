9411b9d2c435575985c3acd9e72778a06ef01d85
Ahmad Izzuddin Azzam <diegomortar@gmail.com>
2025-12-24 09:29:21 +0700

    Setup

---
+++ backend/app/Http/Controllers/SurveyAnalyticsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SurveyAnalytics;
+use Illuminate\Http\Request;
+
+class SurveyAnalyticsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SurveyAnalytics $surveyAnalytics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SurveyAnalytics $surveyAnalytics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SurveyAnalytics $surveyAnalytics)
+    {
+        //
+    }
+}
